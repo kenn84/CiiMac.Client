ae76588732c83b9ed6be4a5b0c9372e312d3cc79
kenn84 <kenneth84petersen@hotmail.com>
2019-06-29 17:39:00 +0200

    Initial Commit

---
+++ CiiMac.MVC/Models/Values.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CiiMac.MVC.Models
+{
+    public class Values
+    {
+        public Values()
+        {
+                
+        }
+        public string ValueOne { get; set; }
+        public string ValueTwo { get; set; }
+    }
+}
