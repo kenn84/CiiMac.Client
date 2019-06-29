58326c6aa938599f668de38bb6b72a595b75918e
kenn84 <kenneth84petersen@hotmail.com>
2019-06-29 17:59:52 +0200

    Values Commit

---
+++ CiiMac.MVC/Controllers/ValueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CiiMac.MVC.Models;
+using CiiMac.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiiMac.MVC.Controllers
+{
+    public class ValueController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+           Task<Values> values = ValueService.GetValuesAsync("https://localhost:44311/api/values");
+            return View();
+        }
+    }
+}
---
+++ CiiMac.MVC/Services/ValueService.cs
@@ -0,0 +1,24 @@
+﻿using CiiMac.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CiiMac.MVC.Services
+{
+    public class ValueService
+    {
+        public static async Task<Values> GetValuesAsync(string path)
+        {
+            HttpClient client = new HttpClient();
+            Values value = new Values();
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                value = await response.Content.ReadAsAsync<Values>();
+            }
+            return value;
+        }
+    }
+}
